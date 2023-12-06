d4061690ec61bb2696b6d67d3126285bae7b64c9
ATLi2001 <42742696+ATLi2001@users.noreply.github.com>
2023-12-06 10:49:01 -0500

    bug fix

--- src/rust_radical_ticket/src/lib.rs
+++ src/rust_radical_ticket/src/lib.rs
@@ -164,7 +164,7 @@ pub fn anti_fraud(_ticket_id: u32, res_email: String, res_name: String, res_card
     for i in 0..feature_vec.len() {
         feature_norm += feature_vec[i].powi(2);
     }
-    feature_norm > 0
+    feature_norm > 0.0
 }
 
 // // reserve a ticket
